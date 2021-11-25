df0a886b5aedbd8f440f8ac8411393fb528c1c4b
jric2002 <joselito18032002@gmail.com>
2021-11-25 12:19:34 -0500

    Codeforces

--- codeforces/watermelon/watermelon.cpp
+++ codeforces/watermelon/watermelon.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, world" << endl;
+  unsigned short int w;
+  cin >> w;
+  if ((w == 2) || ((w % 2) != 0)) {
+    cout << "NO" << endl;
+  }
+  else {
+    cout << "YES" << endl;
+  }
   return 0;
 }
