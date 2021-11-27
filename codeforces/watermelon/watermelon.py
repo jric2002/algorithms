29560451389da8e281e7060cd990087fe410c696
jric2002 <joselito18032002@gmail.com>
2021-11-27 17:30:25 -0500

    Codeforces

---
+++ codeforces/watermelon/watermelon.py
@@ -0,0 +1,5 @@
+w = int(input())
+if ((w == 2) or ((w % 2) != 0)):
+  print("NO")
+else:
+  print("YES")
---
+++ codeforces/way too long words/wtlw.py
@@ -0,0 +1,10 @@
+n = int(input())
+words = []
+for i in range(0, n):
+  words.append(str(input()))
+for i in range(0, n):
+  num_char = len(words[i])
+  if (num_char > 10):
+    print(words[i][0], (num_char - 2), words[i][-1], sep='')
+  else:
+    print(words[i])
