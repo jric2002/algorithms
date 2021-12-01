faefa7c03c2838af1e713e429de2df9bb14bdb6b
jric2002 <joselito18032002@gmail.com>
2021-11-30 19:07:49 -0500

    Codeforces

---
+++ codeforces/6.bit++/bit++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+
+/* Author: José Rodolfo (jric2002) */
+
+using namespace std;
+
+int main() {
+  unsigned short int n;
+  cin >> n;
+  if (n >= 1 && n <= 150) {
+    int x = 0;
+    string statement;
+    while (n--) {
+      cin >> statement;
+      if (statement[1] == '+') {
+        x++;
+      }
+      else if (statement[1] == '-') {
+        x--;
+      }
+      else {
+        continue;
+      }
+    }
+    cout << x << endl;
+  }
+  return 0;
+}
---
+++ codeforces/6.bit++/bit++.py
@@ -0,0 +1,14 @@
+# Author: José Rodolfo (jric2002)
+
+n = int(input())
+if (n >= 1 and n <= 150):
+  x = 0
+  for i in range(0, n):
+    statement = str(input())
+    if (statement[1] == '+'):
+      x += 1
+    elif (statement[1] == '-'):
+      x -= 1
+    else:
+      continue
+  print(x)
