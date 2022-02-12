ab6fd76d044658b825b837ebcba9423bdaddfdd6
jric2002 <joselito18032002@gmail.com>
2022-02-12 14:18:19 -0500

    Codeforces

--- codeforces/37.divisibility_problem/divisibility_problem.cpp
+++ codeforces/37.divisibility_problem/divisibility_problem.cpp
@@ -2,5 +2,22 @@
 /* Author: José Rodolfo (jric2002) */
 using namespace std;
 int main() {
+  int t;
+  unsigned long int a, b;
+  unsigned int movements;
+  cin >> t;
+  unsigned long int min_movements[t];
+  for (int i = 0; i < t; i++) {
+    cin >> a;
+    cin >> b;
+    movements = 0;
+    if ((a % b) != 0) {
+      movements = b - (a % b);
+    }
+    min_movements[i] = movements;
+  }
+  for (int i = 0; i < t; i++) {
+    cout << min_movements[i] << endl;
+  }
   return 0;
 }
--- codeforces/37.divisibility_problem/divisibility_problem.py
+++ codeforces/37.divisibility_problem/divisibility_problem.py
@@ -1,1 +1,11 @@
 # Author: José Rodolfo (jric2002)
+n = int(input())
+min_movements = []
+for i in range(0, n):
+  a, b = [int(number) for number in input().split(" ")]
+  movements = 0
+  if ((a % b) != 0):
+    movements = b - (a % b)
+  min_movements.append(movements)
+for i in range(0, n):
+  print(min_movements[i])
